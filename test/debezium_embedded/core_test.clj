86cb1e5a14ac367368ab2cf13032d5aed7ff581f
Haokang Den <haokang.den@gmail.com>
2019-07-16 11:53:17 +0900

    enable CI

--- test/debezium_embedded/core_test.clj
+++ test/debezium_embedded/core_test.clj
@@ -1,3 +1,28 @@
 (ns debezium-embedded.core-test
   (:require [clojure.test :refer :all]
             [debezium-embedded.core :refer :all]))
+
+(deftest factories
+  (testing "create-configuration"
+    (is (create-configuration {"foo" "bar"})))
+
+  (testing "create-always-offset-commit-policy"
+    (is (create-always-offset-commit-policy)))
+
+  (testing "create-periodic-offset-commit-policy"
+    (is (create-periodic-offset-commit-policy 10)))
+
+  (testing "create-batch-consumer"
+    (is (create-batch-consumer println)))
+
+  (testing "create-completion-callback"
+    (is (create-completion-callback println)))
+
+  (testing "create-connector-callback"
+    (is (create-connector-callback println)))
+
+  (testing "create-engine"
+    (let [config {}
+          consumer (create-batch-consumer println)]
+      (is (create-engine {:config config
+                          :consumer consumer})))))

03445a8f5d3084a134850d9f4b63702c5bcb387c
Haokang Den <haokang.den@gmail.com>
2025-03-29 17:51:53 +0900

    source-record->map

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.12.0"]
+                 [camel-snake-kebab "0.4.3"]
                  [io.debezium/debezium-embedded "3.0.8.Final"]]
   :profiles {:dev {:dependencies [[funcool/promesa "11.0.678"]
+                                  ;; [org.slf4j/slf4j-simple "1.7.36"]
                                   [io.debezium/debezium-connector-postgres "3.0.8.Final"]]}}
   :repl-options {:init-ns debezium-embedded.core})
--- src/debezium_embedded/core.clj
+++ src/debezium_embedded/core.clj
@@ -1,9 +1,12 @@
 (ns debezium-embedded.core
+  (:require [camel-snake-kebab.core :as csk])
   (:import (java.util Properties)
            (io.debezium.embedded EmbeddedEngine$EngineBuilder)
            (io.debezium.engine DebeziumEngine$ChangeConsumer
                                DebeziumEngine$CompletionCallback
-                               DebeziumEngine$ConnectorCallback)))
+                               DebeziumEngine$ConnectorCallback)
+           (org.apache.kafka.connect.source SourceRecord)
+           (org.apache.kafka.connect.data Struct)))
 
 (defn- map->properties [m]
   (let [props (new Properties)]
@@ -11,10 +14,26 @@
       (.setProperty props (name k) (str v)))
     props))
 
+(defn- struct->map [^Struct struct]
+  (when struct
+    (into {}
+          (map (fn [field]
+                 (let [field-name (.name field)]
+                   [(csk/->kebab-case-keyword field-name)
+                    (let [value (.get struct field-name)]
+                      (if (instance? Struct value)
+                        (struct->map value)
+                        value))]))
+               (.fields (.schema struct))))))
+
+(defn- source-record->map [^SourceRecord record]
+  {:offset (into {} (map (fn [[k v]] [(csk/->kebab-case-keyword k) v]) (.sourceOffset record)))
+   :value (struct->map (.value record))})
+
 (defn- create-batch-consumer [f]
   (reify DebeziumEngine$ChangeConsumer
     (handleBatch [_ records committer]
-      (f (into [] records))
+      (f (into [] (map source-record->map) records))
       ;; mark all records as processed
       (doseq [record records]
         (.markProcessed committer record))
@@ -40,16 +59,20 @@
     (taskStopped [_]
       (f ::task-stopped))))
 
+(def default-config
+  {:offset.storage "org.apache.kafka.connect.storage.MemoryOffsetBackingStore"
+   :offset.flush.interval.ms "0"})
+
 (defn create-engine [{:keys [config consumer]
-                      :as options}]
+                      :as arg-map}]
   (let [builder (new EmbeddedEngine$EngineBuilder)]
     ;; Setting required parameters.
     (doto builder
-      (.using (map->properties config))
+      (.using (map->properties (merge default-config config)))
       (.notifying (create-batch-consumer consumer)))
     ;; Setting callbacks.
-    (when-let [callback (get options :completion-callback)]
+    (when-let [callback (get arg-map :completion-callback)]
       (.using builder (create-completion-callback callback)))
-    (when-let [callback (get options :connector-callback)]
+    (when-let [callback (get arg-map :connector-callback)]
       (.using builder (create-connector-callback callback)))
     (.build builder)))
